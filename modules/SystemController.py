c86b924ec0159e5fffc5b2491d0804c902cb0144
Warlax <warlax@hotmail.com>
2016-08-17 08:26:06 +0100

    Added untested WOL and shutdown windows on lan (needs samba-common)

--- modules/SystemController.py
+++ modules/SystemController.py
@@ -1,6 +1,8 @@
 from BaseVoiceControllerModule import BaseVoiceControllerModule
 
 from subprocess import Popen, PIPE, call
+import socket
+import struct
 
 class SystemController(BaseVoiceControllerModule):
     def __init__(self):
@@ -16,11 +18,17 @@ class SystemController(BaseVoiceControllerModule):
             #call(["sudo", "service", "mopidy", "restart"])
 
     def should_action(self, keyword, question):
-        return "bluetooth" in question or "mopidy" in question or "pulseaudio" in question or "speaker" in question
+        return "bluetooth" in question or "mopidy" in question or "pulseaudio" in question or "speaker" in question or "wake" in question
         #return "enable" in question or "disable" in question or "turn on" in question or "turn off" in question or "connect" in question or "restart" in question
 
     def action(self, keyword, question):
-        if "speaker" in question:
+        if "wake" in question:
+            self.response.say("Waking Ash's PC")
+            self.wake_on_lan("10:BF:48:88:FA:99")
+        elif "shutdown" in question:
+            self.response.say("Shutting down Ash's PC")
+            self.shutdown_windows_on_lan("192.168.0.100", "Ash", windows_password)
+        elif "speaker" in question:
             if "bathroom" in question:
                 self.change_speaker("jongos3bathroom_dlna")
             elif "kitchen" in question:
@@ -62,3 +70,33 @@ class SystemController(BaseVoiceControllerModule):
 
     def change_speaker(self, sinkName):
         call(["/home/pi/moveSink.sh", sinkName])
+
+    def wake_on_lan(self, macaddress):
+        """ Switches on remote computers using WOL. """
+
+        # Check macaddress format and try to compensate.
+        if len(macaddress) == 12:
+            pass
+        elif len(macaddress) == 12 + 5:
+            sep = macaddress[2]
+            macaddress = macaddress.replace(sep, '')
+        else:
+            raise ValueError('Incorrect MAC address format')
+     
+        # Pad the synchronization stream.
+        data = ''.join(['FFFFFFFFFFFF', macaddress * 20])
+        send_data = '' 
+
+        # Split up the hex values and pack.
+        for i in range(0, len(data), 2):
+            send_data = ''.join([send_data,
+                                 struct.pack('B', int(data[i: i + 2], 16))])
+
+        # Broadcast it to the LAN.
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.sendto(send_data, ('<broadcast>', 7))
+
+    def shutdown_windows_on_lan(self, ip, user, pasword):
+        call(["net", "rpc", "shutdown", "-I", ip, "-U", user+"%"+password])
+        
