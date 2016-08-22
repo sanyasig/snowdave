569b9d2b0854504108467a460e1d97979b2db418
Warlax <warlax@hotmail.com>
2016-08-22 07:19:52 +0000

    Updated shutdown command now that ssh has been installed on my windows machine

--- modules/SystemController.py
+++ modules/SystemController.py
@@ -113,4 +113,5 @@ class SystemController(BaseVoiceControllerModule):
         sock.sendto(send_data, ('<broadcast>', 7))
 
     def shutdown_windows_on_lan(self, host, user, pasword):
-        call(["net", "rpc", "shutdown", "-I", host, "-U", user+"%"+windows_password])
+        #call(["net", "rpc", "shutdown", "-I", host, "-U", user+"%"+windows_password])
+        call(["ssh", "-l", user, host, "shutdown", "/h"])
